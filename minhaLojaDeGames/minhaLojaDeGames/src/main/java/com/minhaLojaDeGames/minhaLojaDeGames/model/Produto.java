e27fbe6d7dadeef49557ff88eb8c6e9898a38a91
Leonardo <qbleonardo@gmail.com>
2021-02-24 11:13:00 -0300

    Encaminhando criação da Model de Produto

---
+++ minhaLojaDeGames/minhaLojaDeGames/src/main/java/com/minhaLojaDeGames/minhaLojaDeGames/model/Produto.java
@@ -0,0 +1,80 @@
+package com.minhaLojaDeGames.minhaLojaDeGames.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id; 
+	
+	@NotNull
+	@Size(min = 2, max = 300)
+	private String nome;
+	
+	@NotNull
+	private double preco;
+	
+	@Size(min = 2, max = 300)
+	private String desenvolvedora;
+	
+	@OneToMany(mappedBy = "produto", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("produto")
+	private List<Categoria> categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public String getDesenvolvedora() {
+		return desenvolvedora;
+	}
+
+	public void setDesenvolvedora(String desenvolvedora) {
+		this.desenvolvedora = desenvolvedora;
+	}
+
+	public List<Categoria> getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(List<Categoria> categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+}
