fab662da005ba4753c05c61b58be6bea13aa0d30
Leonardo <qbleonardo@gmail.com>
2021-02-24 10:08:09 -0300

    Encaminhando projeto acrescentado o Repository

---
+++ minhaLojaDeGames/minhaLojaDeGames/src/main/java/com/minhaLojaDeGames/minhaLojaDeGames/repository/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.minhaLojaDeGames.minhaLojaDeGames.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.minhaLojaDeGames.minhaLojaDeGames.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	public List<Categoria> findAllGeneroContainingIgnoreCase(String categoria);
+}
